fa6cdd93bde68396dcd155f56b072fd91436fb02
Emma Allegrucci <emmallegrucci@gmail.com>
2021-10-22 14:05:01 -0700

    welcome statement and items for sale printed

--- price_calculator.rb
+++ price_calculator.rb
@@ -4,6 +4,23 @@ def price_calculator
     prices = {"apple" => 0.89, "banana" => 0.99, "bread" => 2.17, "milk" => 3.97}
     groceries = {"apple" => 0, "banana" => 0, "bread" => 0, "milk" => 0}
 
+    puts "Welcome to Emma's Market"
+    puts
+    puts "Here are the items for sale today"
+    puts
+    puts "Item\tUnit price\tSale price"
+    puts "-------------------------------------"
+    prices.each do |key, value|
+        if key == "milk"
+            puts "#{key}\t$#{value}\t\t2 for $5.00"
+        elsif key == "bread"
+            puts "#{key}\t$#{value}\t\t3 for $6.00"
+        else
+            puts "#{key}\t$#{value}"
+        end
+    end
+    puts
+
     puts "Please enter all the items purchased seperated by a comma"
     result = gets
     items = result.split(",")
