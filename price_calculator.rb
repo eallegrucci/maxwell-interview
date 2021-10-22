e3a8e4467558cca2853ff3cc5a39446af59aa7de
Emma Allegrucci <emmallegrucci@gmail.com>
2021-10-22 14:08:38 -0700

    total and saved amount calculated

--- price_calculator.rb
+++ price_calculator.rb
@@ -4,7 +4,7 @@ def price_calculator
     prices = {"apple" => 0.89, "banana" => 0.99, "bread" => 2.17, "milk" => 3.97}
     groceries = {"apple" => 0, "banana" => 0, "bread" => 0, "milk" => 0}
     itemCost = {}
-    
+
     puts "Welcome to Emma's Market"
     puts
     puts "Here are the items for sale today"
@@ -38,23 +38,29 @@ def price_calculator
         end
     end
 
+    saved = 0
+    total = 0
     groceries.each do |key, value|
         if value > 0
             if key == "bread"
                 n = value/3
                 m = value%3
                 itemCost[key] = (prices[key] * m) + (6 * n)
+                saved += (prices[key] * value) - itemCost[key]
             elsif key == "milk"
                 n = value/2
                 m = value%2
                 itemCost[key] = (prices[key] * m) + (5 * n)
+                saved += (prices[key] * value) - itemCost[key]
             else
                 itemCost[key] = prices[key] * value
             end
+            total += itemCost[key]
         end
     end
 
-    puts itemCost
+    puts total
+    puts saved
 end
 
 price_calculator
